58d6e2ad41bd7eb95b108e089c6d986c42a11e59
unknown <sid1331@gmail.com>
2015-02-23 16:24:04 +0100

    typeahead initialization in the init instead of construct

--- js/ui/NodeEditPanel.js
+++ js/ui/NodeEditPanel.js
@@ -72,13 +72,6 @@ module.exports = function (Nodium, $, _, undefined) {
                 },
                 queryTokenizer: Bloodhound.tokenizers.whitespace
             });
-
-            $('#new-edge-target').typeahead(null, {
-                source:     this.bloodhound.ttAdapter(),
-                displayKey: function (node) {
-                    return Node.getPropertyValue(node, 'name');
-                }
-            });
         },
 
         init: function (container) {
@@ -108,10 +101,19 @@ module.exports = function (Nodium, $, _, undefined) {
 
             $('#delete-node-button', this.view).on(Event.CLICK, this.handleDeleteNodeButtonClick.bind(this));
 
+            $('#new-edge-target').typeahead(null, {
+                source:     this.bloodhound.ttAdapter(),
+                displayKey: function (node) {
+                    return Node.getPropertyValue(node, 'name');
+                }
+            });
+
             return this;
         },
 
         getTypeaheadNodes: function () {
+
+            console.log('TYPEAHEADING');
 
             // filter all nodes
             if (this.nodes) {
